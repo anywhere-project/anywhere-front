d05e69cdc23953f9129fbc9ab81794c8ffedc448
workmakerdaily <sver12@naver.com>
2024-12-02 15:57:50 +0900

    second

---
+++ src/Anywhere.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Anywhere.css';
+
+
+
+function Anywhere() {
+  return (
+    <div>Anywhere</div>
+  );
+}
+
+export default Anywhere;
