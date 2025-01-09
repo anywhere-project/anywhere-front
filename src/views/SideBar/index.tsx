e23e5b3d006ac8ab3c4a466cf7126fd664c6b1ac
workmakerdaily <sver12@naver.com>
2025-01-09 21:51:14 +0900

    25.01.09 21:51

--- src/views/SideBar/index.tsx
+++ src/views/SideBar/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css';
 
 export default function SideBar() {
+
+    const [barPosition, setBarPosition] = useState<number>(670);
+
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY; 
+        const height = window.innerHeight
+        const newTop = Math.max(670, scrollPosition + height / 2 - 100);
+        setBarPosition(newTop);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <div id="side-wrapper">
-            <div className="side-container">
+            <div className="side-container" style={{ top: `${barPosition}px` }}>
                 <div className="menu-list">
                     <div className="menu-item">메인</div>
                     <div className="menu-item">후기게시판</div>
@@ -12,6 +29,5 @@ export default function SideBar() {
                     <div className="menu-item">마이페이지</div>
                 </div>
             </div>
-        </div>
     );
-}
+};
